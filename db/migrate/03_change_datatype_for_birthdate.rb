d770b301e49902fe701e69345c621875a6fb5590
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 13:21:59 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,8 @@
 class ChangeDatatypeForBirthdate < ActiveRecord[5.1]
   
-  def change_column(
-    :students, :birthdate, :datetime)
+  def change 
+    change_column
+    :students, :birthdate, :datetime
+  end
   
 end 
