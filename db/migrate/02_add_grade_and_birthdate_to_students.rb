ed40c1e7266666f74c1773ffa387c710d579a059
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 13:14:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,8 +2,8 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
     add_column :students,
-      :grade, :integer,
-      :birthdate, :string 
+      :grade, integer,
+      :birthdate, string 
   end
   
 end
